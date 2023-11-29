8579d1c9a5ce958a573b18f918cbfced720e06f1
Luis <lueduar15@gmail.com>
2023-11-29 00:13:51 -0400

    enter information

--- script/registration-form-script.js
+++ script/registration-form-script.js
@@ -1,4 +1,5 @@
 
+
 const form = document.getElementById("form");
 const nameField = document.getElementById("name");
 const lastNameField = document.getElementById("lastname");
@@ -13,19 +14,19 @@ form.addEventListener("submit", e=>{
     let emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
     paragraph.innerHTML = ""
     if(nameField.value.length <2){
-        warnings += `Invalid name <br>`
+        warnings += `Enter name <br>`
         getIn = true
     }
     if(lastNameField.value.length <3){
-        warnings += `Invalid last name <br>`
+        warnings += `Enter last name <br>`
         getIn = true
     }
     if(!emailValidation.test(emailField.value)){
-        warnings += `Invalid email <br>`
+        warnings += `Enter email <br>`
         getIn = true
     }
     if(passwordField.value.length <8){
-        warnings += `Invalid password <br>`
+        warnings += `Enter password <br>`
         getIn = true
     }
     if(getIn){
